9c15409b111468c0ab1117b1b24ac3d962719370
Brian <mccabj0210@gmail.com>
2014-12-10 11:35:51 -0800

    switched to ngmaps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,49 +8,78 @@
  * Controller of the bikeMapApp
  */
 angular.module('bikeMapApp')
-  .controller('MainCtrl', function ($scope, Attr2Options) {
+  .controller('MainCtrl', function ($scope) {
 
     var kmlUrl = 'http://www.mcshiz.com/bike-map/layers/gateway.kmz';
     var kmlUrl2 = 'http://www.mcshiz.com/bike-map/layers/eight-mile.kmz';
-  	
+ 
+  $scope.$on('mapInitialized', function(event, map) {
+  $scope.map = { 
+    coords: '41.349700, -122.312284',
+    zoom: '12',
+  }
 
+   var layer = [
+    {
+      name: 'Eight-mile',
+      url: 'http://www.mcshiz.com/bike-map/layers/eight-mile.kmz', 
+      info: 'this is the 8-mile trail... its a classic'
+    },
+    {
+      name: 'Gateway',
+      url: 'http://www.mcshiz.com/bike-map/layers/gateway.kmz',
+      info: 'This is the gateway loop..most pop xc trail'
+    }]
 
-$scope.map = { 
-      coords: '41.349700, -122.312284',
-		zoom: '12',
+    var lay = [];
+    $.each(layer, function(index, val) {
+     lay[index] = new google.maps.KmlLayer({
+      url: val.url,
+      options: {'suppressInfoWindows' : true},
+      info: val.info
+    });
+    lay[index].setMap(map);
+
+    google.maps.event.addListener(lay[index], 'click', function(kmlEvent) {
+      showInContextWindow(val.info);
+    });
+  });
+
+  $scope.getLocation = function(){
+    if(navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(function(position) {
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.panTo(latLng);
+    })
+  }
 }
-  $scope.layers = 
-	    [
-	    	{
-		    	url: kmlUrl,
-	    	},
-	    	{
-		    	url: kmlUrl2,
-	    	}
-	    ] 
-$scope.showInContextWindow = function(event) {
-      $scope.description = event.featureData.description;
-      console.log("woah dude")
-    };
 
-
+var watchID = null;
 
 $scope.getLocation = function(){
-	  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      $scope.map.coords = position.coords.latitude+","+position.coords.longitude;
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
+  var options = {frequency: 5000};
+  watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
 }
-
+function onSuccess(position){
+  
 }
 
 
 
 
 
-  });
+  }); //end mapInitialized
+
+
+
+
+  var showInContextWindow = function(info) {
+    $('.infoWindow').text(info);
+  };//end showincontext
+
+
+
+
+
+
+  });//end mainCtrl
