89f7f2efb063a8c08f036747200b25b875e466e7
Brian <mccabj0210@gmail.com>
2014-12-12 16:41:56 -0800

    added multiple geoxml kml parse option

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,8 +16,8 @@ angular.module('bikeMapApp')
     zoom: '12',
   }
 var geoXml = null;
-var geoXmlDoc = null;
-
+var geoXmlDoc = [];
+var layers = ['/layers/gateway.kml', '/layers/doc.kml']
 
 
    geoXml = new geoXML3.parser({
@@ -27,7 +27,7 @@ var geoXmlDoc = null;
                     afterParse: useTheData
                 });
 
-geoXml.parse('/layers/doc.kml');
+geoXml.parse(layers);
 
 
 
@@ -38,10 +38,13 @@ function useTheData(doc){
   // Geodata handling goes here, using JSON properties of the doc object
  
 //  var sidebarHtml = '<table>';
-  geoXmlDoc = doc[0];
-  for (var i = 0; i < geoXmlDoc.placemarks.length; i++) {
+for (var l = 0; l<doc.length; l++){
+
+
+  geoXmlDoc[l] = doc[l];
+  for (var i = 0; i < geoXmlDoc[l].placemarks.length; i++) {
     // console.log(doc[0].markers[i].title);
-    var placemark = geoXmlDoc.placemarks[i];
+    var placemark = geoXmlDoc[l].placemarks[i];
 
     if (placemark.polyline) {
       var normalStyle = {
@@ -50,129 +53,67 @@ function useTheData(doc){
           strokeOpacity: placemark.polyline.get('strokeOpacity')
           };
       placemark.polyline.normalStyle = normalStyle;
-console.log(placemark.polyline)
-      highlightPoly(placemark.polyline, i);
-}
-    if (placemark.marker) {
-      if (placemark.marker.id == "start") {
-      placemark.marker.setOptions({icon: "images/start-flag.png"})
-    }
-      if (placemark.marker.id == "end") {
-      placemark.marker.setOptions({icon: "images/end-flag.png"})
-    }
-      hideMarker(placemark.marker, i);
+      highlightPoly(placemark.polyline, i, geoXmlDoc[l] );
     }
 
-/*    doc[0].markers[i].setVisible(false); */
-  }
+    if (placemark.marker) {
+      if (placemark.marker.id == "start") {
+        placemark.marker.setOptions({icon: "images/start-flag.png"})
+      }
+      if (placemark.marker.id == "end") {
+        placemark.marker.setOptions({icon: "images/end-flag.png"})
+      }
+      if (placemark.marker.id == "loop") {
+        placemark.marker.setOptions({icon: "images/loop-flag.png"})
+      }    
+    }// end if marker
+
+  }//end placemarks for loop
+}//end doc for loop
 
 };
 
 var highlightOptions = {fillColor: "#FFFFFF",Color: "#FFFFFF", strokeColor: "#000000", fillOpacity: 0.9, strokeWidth: 10};
 var highlightLineOptions = {strokeColor: "#fa5519", strokeWidth: 10, color: "#ffffff"};
 
-function hideMarker(marker2){
- google.maps.event.addListener(marker2, 'click', function() {
-  marker2.setOptions({icon: "/images/start-flag.png"})
-  })
-}
-
-function kmlHighlightPoly(pm) {
-   for (var i=0;i<geoXmlDoc.placemarks.length;i++) {
-     var placemark = geoXmlDoc.placemarks[i];
-     if (i == pm) {
-       if (placemark.polygon) placemark.polygon.setOptions(highlightOptions);
-       if (placemark.polyline) placemark.polyline.setOptions(highlightLineOptions);
-     } else {
-       if (placemark.polygon) placemark.polygon.setOptions(placemark.polygon.normalStyle);
-       if (placemark.polyline) placemark.polyline.setOptions(placemark.polyline.normalStyle);
-     }
-   }
-}
-function kmlUnHighlightPoly(pm) {
-   for (var i=0;i<geoXmlDoc.placemarks.length;i++) {
-     if (i == pm) {
-       var placemark = geoXmlDoc.placemarks[i];
-       if (placemark.polygon) placemark.polygon.setOptions(placemark.polygon.normalStyle);
-       if (placemark.polyline) placemark.polyline.setOptions(placemark.polyline.normalStyle);
-     }
-   }
-}
-
 var on = false;
-function highlightPoly(poly, polynum) {
-  //    poly.setOptions({fillColor: "#0000FF", strokeColor: "#0000FF", fillOpacity: 0.3}) ;
+function highlightPoly(poly, polynum, layer) {
   google.maps.event.addListener(poly,"click",function() {
+
+    $scope.brian = poly;
     if (on === false){
       poly.setOptions(highlightLineOptions); 
-        on = !on;
-
-        //create custom kml tags on 404 of geoxml3
-        $('#sidr .layer-title').text(poly.title);
-        $('#sidr .layer-info').html(poly.distance + "<br>" + poly.description);
-        $.sidr('open');
+      on = !on;
+      //create custom kml tags on 404 of geoxml3
+      showInContextWindow(poly.title, poly.description, poly.distance)
+      $.sidr('open');
     } else {
-      poly.setOptions(poly.normalStyle);
-        on = !on;
-        $.sidr('close');
+      $scope.unHighlightPoly(poly)
+      $scope.sidrClose(on, poly)
     }
   });
 
-  google.maps.event.addListener(poly,"mouseout",function() {
-    
+  google.maps.event.addListener(poly,"mouseout",function() { 
   });
-}  
+}
 
-
-function kmlPgClick(pm) {
-   if (geoXml.docs[0].placemarks[pm].polygon.getMap()) {
-      google.maps.event.trigger(geoXmlDoc.placemarks[pm].polygon,"click");
-   } else {
-      geoXmlDoc.placemarks[pm].polygon.setMap(map);
-      google.maps.event.trigger(geoXmlDoc.placemarks[pm].polygon,"click");
-   }
+$scope.unHighlightPoly = function(poly) {
+  poly.setOptions(poly.normalStyle);
+  console.log(poly)
+   on = !on;
 }
 
 
 
+
 // =======================================================================================================================
 // =======================================================================================================================
 // =======================================================================================================================
-   var layer = [
-    {
-      name: 'Eight-mile',
-      url: 'http://www.mcshiz.com/bike-map/layers/gateway.kmz', 
-      info: 'this is the 8-mile trail... its a classic'
-    },
-    {
-      name: 'Gateway',
-      url: 'http://www.mcshiz.com/bike-map/layers/gateway.kmz',
-      info: 'This is the gateway loop.most pop xc trail',
-    }]
-
-    var lay = [];
-    $.each(layer, function(index, val) {
-     lay[index] = new google.maps.KmlLayer({
-      url: val.url,
-      options: {'suppressInfoWindows' : true},
-      info: val.info
-    });
-    lay[index].setMap(map);
-
-    google.maps.event.addListener(lay[index], 'click', function(kmlEvent) {
-      showInContextWindow(val.name, val.info);
-    });
-  });
 
 
-  $('#simple-menu').sidr({
-      displace: false,
-    })
-  $('.sidr-close').on('click', function(){
-    $.sidr('close');
-    $scope.sidrOpen = false;
-});
-	$scope.tracking = false;
+
+$scope.tracking = false;
+
 $scope.getLocation = function(){
 	$scope.tracking = !$scope.tracking;
 	locationBtn();
@@ -188,38 +129,26 @@ var findLocation = function(value){
   if (value === true) {
   	$scope.locationMarker = null;
     if (navigator.geolocation) {
-      
       navigator.geolocation.getCurrentPosition(
-               function( position ){
-                    if ($scope.locationMarker){
-                        return;
-                    }
-
-                    // Add a marker to the map using the position.
-                    $scope.locationMarker = addMarker(
-                        position.coords.latitude,
-                        position.coords.longitude
-                    );
-
-                },
-          function( error ){
-              console.log( "Something went wrong: ", error );
-          },
-          {
-              timeout: (5 * 1000),
-              maximumAge: (1000 * 60 * 15),
-              enableHighAccuracy: true
+        function( position ){
+          if ($scope.locationMarker){
+            return;
           }
-      );
+          // Add a marker to the map using the position.
+          $scope.locationMarker = addMarker(position.coords.latitude,position.coords.longitude);
+        },
+        function( error ){
+          console.log( "Something went wrong: ", error );
+        },
+        {
+          timeout: (5 * 1000),
+          maximumAge: (1000 * 60 * 15),
+          enableHighAccuracy: true
+        });
 
       $scope.positionTimer = navigator.geolocation.watchPosition(
           function( position ){
-              updateMarker(
-                  $scope.locationMarker,
-                  position.coords.latitude,
-                  position.coords.longitude
-              );
-
+              updateMarker($scope.locationMarker, position.coords.latitude,position.coords.longitude);
           }
       );
 
@@ -279,23 +208,38 @@ function updateMapPosition(latitude, longitude){
 
   }); //end mapInitialized
 
+$('#simple-menu').sidr({
+    displace: false,
+  })
 
 
-  $scope.sidrOpen = false;
-  var showInContextWindow = function(title, info) {
-    $('#sidr .layer-title').text(title);
-    $('#sidr .layer-info').text(info);	
-    console.log($scope.sidrOpen)
-    if ($scope.sidrOpen !== true){
-	    $.sidr(function(){
-	    	$scope.sidrOpen = true
-	    })
-      $scope.sidrOpen = true;
-    };
-  };//end showincontext
+$scope.sidrOpen = false;
 
 
+var showInContextWindow = function(title, description, distance) {
+  $('#sidr .layer-title').text(title);
+  $('#sidr .layer-description').text(description);	
+  $('#sidr .layer-distance').text(distance); 
 
+  if ($scope.sidrOpen !== true){
+    $.sidr(function(){
+  	  $scope.sidrOpen = true
+    });
+    $scope.sidrOpen = true;
+  }
+};//end showincontext
+
+
+$scope.sidrClose = function(poly){
+  if (poly){
+    $scope.unHighlightPoly(poly);
+  }
+  $.sidr('close');
+  $('#sidr .layer-title').text("");
+  $('#sidr .layer-description').text("");
+  $('#sidr .layer-distance').text("");
+  $scope.sidrOpen = false;  
+}
 
 
 
